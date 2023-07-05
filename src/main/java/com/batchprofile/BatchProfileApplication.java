ab324a2a4bc1d6835375e27b8f1361daa9e1f0db
Michamoro <mimoro1176@hotmail.com>
2023-07-05 15:14:47 -0500

    initial commit

---
+++ src/main/java/com/batchprofile/BatchProfileApplication.java
@@ -0,0 +1,13 @@
+package com.batchprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batchprofile/BatchProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
